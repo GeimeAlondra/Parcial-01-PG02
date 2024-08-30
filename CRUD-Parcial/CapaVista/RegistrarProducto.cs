98f422a34b535e6078652026f990a7fc01eb0f70
GeimeAlondra <105683683+GeimeAlondra@users.noreply.github.com>
2024-08-29 21:18:21 -0600

    Comentarios de CapaVista

--- CRUD-Parcial/CapaVista/RegistrarProducto.cs
+++ CRUD-Parcial/CapaVista/RegistrarProducto.cs
@@ -16,24 +16,39 @@ namespace CapaVista
 {
     public partial class RegistrarProducto : Form
     {
+        // Declara una instancia del repositorio de productos
         ProductoRepository _productoRepository;
+
+        // Declara una variable para almacenar el ID del producto
         int id;
+
+        // Declara una referencia al formulario VerProducto
         private VerProducto _form1;
 
+        // Constructor de la clase RegistrarProducto
         public RegistrarProducto(VerProducto form1, int _id = 0)
         {
+            // Inicializa los componentes del formulario
             InitializeComponent();
+
+            // Asigna el valor del ID pasado al constructor
             id = _id;
 
+            // Si se pasa un ID mayor a 0, carga los datos del producto con ese ID
             if (id > 0)
             {
+                // Llama a un método para cargar los datos del producto en los campos del formulario
                 CargarCampos(id);
             }
             else
             {
+                // Mueve el BindingSource a la última posición (probablemente no aplicable aquí ya que es una nueva instancia)
                 productosBindingSource.MoveLast();
+
+                // Añade un nuevo registro al BindingSource
                 productosBindingSource.AddNew();
 
+                // Crea una nueva instancia de Producto y la asigna como la fuente de datos del BindingSource
                 Producto producto = new Producto();
                 productosBindingSource.DataSource = producto;
             }
@@ -42,8 +57,10 @@ namespace CapaVista
         // Métodos para cargar el datagrid despues de realizar una inserción o actualización
         public event EventHandler LlenarDataGridViewRequested;
 
+        // Método que activa el evento LlenarDataGridViewRequested
         private void OnLlenarDataGridViewRequested()
         {
+            // Invoca el evento si tiene suscriptores
             LlenarDataGridViewRequested?.Invoke(this, EventArgs.Empty);
         }
 
@@ -54,150 +71,209 @@ namespace CapaVista
 
         }
 
+        // Método manejador del evento Click del botón Cancelar
         private void btnCancelar_Click(object sender, EventArgs e)
         {
+            // Cierra el formulario actual
             this.Close();
         }
 
+        // Método para cargar los datos del producto en los campos del formulario
         private void CargarCampos(int id)
         {
+            // Inicializa la instancia del repositorio de productos
             _productoRepository = new ProductoRepository();
+
+            // Obtiene el producto con el ID especificado y establece el producto en el BindingSource
             productosBindingSource.DataSource = _productoRepository.ObtenerPorID(id);
         }
 
+        // Método manejador del evento Click del botón Guardar
         private void btnGuardar_Click(object sender, EventArgs e)
         {
+            // Llama al método GuardarProducto para guardar los datos del producto
             GuardarProducto();
         }
 
+        // Método para validar los campos del formulario
         private bool ValidarCampos()
         {
+            // Inicializa un indicador de validez de campos como verdadero
             bool camposValidos = true;
 
+            // Verifica si el campo Nombre está vacío
             if (string.IsNullOrEmpty(txtNombre.Text))
             {
+                // Muestra un mensaje de advertencia si el campo está vacío
                 MessageBox.Show("Se requiere el nombre del producto", "| Registro Producto", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                // Enfoca el campo Nombre para que el usuario pueda ingresarlo
                 txtNombre.Focus();
+                // Marca los campos como inválidos
                 camposValidos = false;
             }
 
+            // Verifica si el campo Descripción está vacío
             if (string.IsNullOrEmpty(txtDescripcion.Text))
             {
+                // Muestra un mensaje de advertencia si el campo está vacío
                 MessageBox.Show("Se requiere la descripción del producto", "| Registro Producto", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                // Enfoca el campo Descripción para que el usuario pueda ingresarlo
                 txtDescripcion.Focus();
+                // Marca los campos como inválidos
                 camposValidos = false;
             }
 
+            // Verifica si el campo Precio está vacío
             if (string.IsNullOrEmpty(txtPrecio.Text))
             {
+                // Muestra un mensaje de advertencia si el campo está vacío
                 MessageBox.Show("Se requiere el precio del producto", "| Registro Producto", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                // Enfoca el campo Precio para que el usuario pueda ingresarlo
                 txtPrecio.Focus();
+                // Marca los campos como inválidos
                 camposValidos = false;
             }
 
+            // Verifica si el campo Stock está vacío
             if (string.IsNullOrEmpty(txtStock.Text))
             {
+                // Muestra un mensaje de advertencia si el campo está vacío
                 MessageBox.Show("Se requiere el stock del producto", "| Registro Producto", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                // Enfoca el campo Stock para que el usuario pueda ingresarlo
                 txtStock.Focus();
+                // Marca los campos como inválidos
                 camposValidos = false;
             }
 
+            // Verifica si el campo Categoría está vacío
             if (string.IsNullOrEmpty(txtCategoria.Text))
             {
-                MessageBox.Show("Se requiere el precio del producto", "| Registro Producto", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                // Muestra un mensaje de advertencia si el campo está vacío
+                MessageBox.Show("Se requiere la categoría del producto", "| Registro Producto", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                // Enfoca el campo Categoría para que el usuario pueda ingresarlo
                 txtCategoria.Focus();
+                // Marca los campos como inválidos
                 camposValidos = false;
             }
 
+            // Verifica si el campo Marca está vacío
             if (string.IsNullOrEmpty(txtMarca.Text))
             {
+                // Muestra un mensaje de advertencia si el campo está vacío
                 MessageBox.Show("Se requiere la marca del producto", "| Registro Producto", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                // Enfoca el campo Marca para que el usuario pueda ingresarlo
                 txtMarca.Focus();
+                // Marca los campos como inválidos
                 camposValidos = false;
             }
 
+            // Devuelve si todos los campos son válidos
             return camposValidos;
         }
 
+        // Método para guardar o actualizar un producto
         private void GuardarProducto()
         {
+            // Inicializa el repositorio de productos
             _productoRepository = new ProductoRepository();
+
             try
             {
+                // Valida los campos del formulario
                 if (!ValidarCampos())
                 {
-                    return; // Si los campos no son válidos, se sale del metodo
+                    // Si los campos no son válidos, sale del método
+                    return;
                 }
+
                 int resultado;
-               
-                // Indicamos si guardaremos o actualizaremos
+
+                // Verifica si se está actualizando un producto existente (id > 0) o agregando uno nuevo
                 if (id > 0)
                 {
+                    // Finaliza la edición del BindingSource para aplicar los cambios
                     productosBindingSource.EndEdit();
-                    Producto producto;
-                    producto = (Producto)productosBindingSource.Current;
+
+                    // Obtiene el producto actual del BindingSource
+                    Producto producto = (Producto)productosBindingSource.Current;
+
+                    // Llama al método para actualizar el producto en la base de datos
                     resultado = _productoRepository.ActualizarProducto(producto);
+
                     if (resultado > 0)
                     {
+                        // Limpia los campos del formulario después de una actualización exitosa
                         txtNombre.Clear();
                         txtDescripcion.Clear();
                         txtPrecio.Clear();
                         txtStock.Clear();
                         txtCategoria.Clear();
                         txtMarca.Clear();
-                        MessageBox.Show("Producto actualizado con exito", "| Registro Producto",
-                            MessageBoxButtons.OK, MessageBoxIcon.Information);
 
+                        // Muestra un mensaje de éxito y cierra el formulario
+                        MessageBox.Show("Producto actualizado con éxito", "| Registro Producto",
+                            MessageBoxButtons.OK, MessageBoxIcon.Information);
                         this.Close();
                     }
                     else
                     {
-                        MessageBox.Show("No se logro actualizar el producto", "| Registro Producto",
+                        // Muestra un mensaje de error si la actualización falla
+                        MessageBox.Show("No se logró actualizar el producto", "| Registro Producto",
                             MessageBoxButtons.OK, MessageBoxIcon.Error);
                     }
                 }
                 else
                 {
+                    // Finaliza la edición del BindingSource para aplicar los cambios
                     productosBindingSource.EndEdit();
 
-                    Producto producto;
-                    producto = (Producto)productosBindingSource.Current;
+                    // Obtiene el producto actual del BindingSource
+                    Producto producto = (Producto)productosBindingSource.Current;
 
+                    // Llama al método para guardar un nuevo producto en la base de datos
                     resultado = _productoRepository.GuardarProducto(producto);
 
                     if (resultado > 0)
                     {
+                        // Limpia los campos del formulario después de agregar el producto
                         txtNombre.Clear();
                         txtDescripcion.Clear();
                         txtPrecio.Clear();
                         txtStock.Clear();
                         txtCategoria.Clear();
                         txtMarca.Clear();
-                        MessageBox.Show("Producto agregado con exito", "| Registro Producto",
+
+                        // Muestra un mensaje de éxito y cierra el formulario
+                        MessageBox.Show("Producto agregado con éxito", "| Registro Producto",
                             MessageBoxButtons.OK, MessageBoxIcon.Information);
                         this.Close();
                     }
                     else
                     {
-                        MessageBox.Show("No se logro guardar el Producto", "| Registro Producto",
+                        // Muestra un mensaje de error si el guardado falla
+                        MessageBox.Show("No se logró guardar el producto", "| Registro Producto",
                             MessageBoxButtons.OK, MessageBoxIcon.Error);
                     }
-
                 }
+
+                // Dispara el evento para llenar el DataGridView
                 OnLlenarDataGridViewRequested();
             }
             catch (Exception ex)
             {
-                MessageBox.Show($"Ocurrio un Error: {ex}", "| Registro Producto",
+                // Muestra un mensaje de error si ocurre una excepción
+                MessageBox.Show($"Ocurrió un error: {ex.Message}", "| Registro Producto",
                    MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
         }
 
+        // Validación para permitir solo caracteres enteros y punto en el textBox de Stock
         private void txtPrecio_KeyPress(object sender, KeyPressEventArgs e)
         {
             // Permitir solo dígitos, el punto decimal y el carácter de retroceso
             if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) && e.KeyChar != '.')
             {
+                // Si no es válido, cancela el carácter (evita que se ingrese)
                 e.Handled = true;
             }
         }
@@ -205,17 +281,21 @@ namespace CapaVista
         // Validación para permitir solo caracteres enteros en el textBox de Stock
         private void txtStock_KeyPress(object sender, KeyPressEventArgs e)
         {
+            // Verifica si el carácter ingresado no es un control (como Backspace) y no es un dígito
             if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar))
             {
+                // Si no es válido, cancela el carácter (evita que se ingrese)
                 e.Handled = true;
             }
         }
 
-        // Validación para permitir solo caracteres alfabeticos en el textBox de Categoria
+        // Validación para permitir solo caracteres alfabéticos en el textBox de Categoria
         private void txtCategoria_KeyPress(object sender, KeyPressEventArgs e)
         {
+            // Verifica si el carácter ingresado no es un control (como Backspace), no es una letra y no es un espacio en blanco
             if (!char.IsControl(e.KeyChar) && !char.IsLetter(e.KeyChar) && !char.IsWhiteSpace(e.KeyChar))
             {
+                // Si no es válido, cancela el carácter (evita que se ingrese)
                 e.Handled = true;
             }
         }
--- CRUD-Parcial/CapaVista/VerProducto.cs
+++ CRUD-Parcial/CapaVista/VerProducto.cs
@@ -13,91 +13,138 @@ namespace CapaVista
 {
     public partial class VerProducto : Form
     {
+        // Instancia de ProductoRepository para manejar operaciones con productos
         ProductoRepository _productoRepository;
 
+        // Constructor de la clase VerProducto
         public VerProducto()
         {
-            InitializeComponent();
-            CargarProductos();
-            _productoRepository = new ProductoRepository();
+            // Inicializa los componentes del formulario
+            InitializeComponent(); 
+
+            // Llama a un método para cargar los productos
+            CargarProductos();  
+            
+            // Inicializa la instancia de ProductoRepository
+            _productoRepository = new ProductoRepository();  
         }
 
+        // Maneja el evento de clic en el botón de guardar en el BindingNavigator
         private void productosBindingNavigatorSaveItem_Click(object sender, EventArgs e)
         {
+            // Valida los datos del formulario
             this.Validate();
-            this.productosBindingSource.EndEdit();
-            this.tableAdapterManager.UpdateAll(this.parcial01DataSet);
 
+            // Finaliza la edición de los datos en el BindingSource
+            this.productosBindingSource.EndEdit();
+
+            // Actualiza todos los cambios en el DataSet a la base de datos
+            this.tableAdapterManager.UpdateAll(this.parcial01DataSet);  
         }
 
+        // Maneja el evento de carga del formulario
         private void VerProducto_Load(object sender, EventArgs e)
         {
-            // TODO: esta línea de código carga datos en la tabla 'parcial01DataSet.Productos'
-            this.productosTableAdapter.Fill(this.parcial01DataSet.Productos);
-
+            // Carga los datos de la tabla 'Productos' en el DataSet 'parcial01DataSet'
+            this.productosTableAdapter.Fill(this.parcial01DataSet.Productos);  
         }
 
+        // Método para cargar todos los productos en el DataGrid
         private void CargarProductos()
         {
+            // Inicializa la instancia de ProductoRepository
             _productoRepository = new ProductoRepository();
-            productosDataGrid.DataSource = _productoRepository.ObtenerTodos();
+
+            // Asigna la lista de productos al DataSource del DataGrid
+            productosDataGrid.DataSource = _productoRepository.ObtenerTodos();  
         }
 
+        // Método para filtrar los productos por nombre
         private void Filtrar()
         {
+            // Inicializa la instancia de ProductoRepository
             _productoRepository = new ProductoRepository();
+
+            // Obtiene el texto del TextBox para buscar
             string nombre = txtBuscar.Text;
-            productosDataGrid.DataSource = _productoRepository.FiltroNombre(nombre);
+
+            // Asigna la lista filtrada al DataSource del DataGrid
+            productosDataGrid.DataSource = _productoRepository.FiltroNombre(nombre);  
         }
 
+        // Maneja el evento de cambio de texto en el TextBox de búsqueda
         private void txtBuscar_TextChanged(object sender, EventArgs e)
         {
-            Filtrar();
+            // Llama al método Filtrar cuando el texto del TextBox cambia
+            Filtrar();  
         }
 
+        // Maneja el evento de clic en el botón Agregar
         private void btnAgregar_Click(object sender, EventArgs e)
         {
+            // Crea una nueva instancia del formulario RegistrarProducto, pasando la instancia actual (this) como parámetro
             RegistrarProducto objRegistroProducto = new RegistrarProducto(this);
+
+            // Suscribe al evento LlenarDataGridViewRequested del formulario RegistrarProducto para manejar la actualización del DataGridView
             objRegistroProducto.LlenarDataGridViewRequested += Form1_LlenarDataGridViewRequested;
+
+            // Muestra el formulario RegistrarProducto
             objRegistroProducto.Show();
         }
 
+        // Maneja el evento LlenarDataGridViewRequested para actualizar el DataGridView
         private void Form1_LlenarDataGridViewRequested(object sender, EventArgs e)
         {
-            // Actualizar el DataGridView
+            // Llama al método CargarProductos para actualizar el DataGridView con los datos más recientes
             CargarProductos();
         }
 
+        // Maneja el evento de clic en las celdas del DataGridView
         private void productosDataGrid_CellClick(object sender, DataGridViewCellEventArgs e)
         {
+            // Verifica si la columna clickeada es la de editar
             if (productosDataGrid.Columns[e.ColumnIndex].Name == "btnEditar")
             {
+                // Obtiene el Id del producto desde la celda de la fila actual
                 int Id = Convert.ToInt32(productosDataGrid.CurrentRow.Cells["Id"].Value.ToString());
 
+                // Crea una nueva instancia del formulario RegistrarProducto para editar el producto, pasando el Id como parámetro
                 RegistrarProducto objRegistroProducto = new RegistrarProducto(this, Id);
+
+                // Suscribe al evento LlenarDataGridViewRequested del formulario RegistrarProducto para manejar la actualización del DataGridView
                 objRegistroProducto.LlenarDataGridViewRequested += Form1_LlenarDataGridViewRequested;
+
+                // Muestra el formulario RegistrarProducto
                 objRegistroProducto.Show();
             }
-            else if(productosDataGrid.Columns[e.ColumnIndex].Name == "btnEliminar")
+            // Verifica si la columna clickeada es la de eliminar
+            else if (productosDataGrid.Columns[e.ColumnIndex].Name == "btnEliminar")
             {
+                // Obtiene el Id del producto desde la celda de la fila actual
                 int Id = Convert.ToInt32(productosDataGrid.CurrentRow.Cells["Id"].Value.ToString());
+
+                // Crea una nueva instancia del repositorio de productos
                 _productoRepository = new ProductoRepository();
-                _productoRepository.EliminarProducto(Id);
+
+                // Llama al método EliminarProducto para eliminar el producto con el Id especificado
                 int resultado = _productoRepository.EliminarProducto(Id);
 
-                if (resultado == 0)
+                // Muestra un mensaje de éxito o error basado en el resultado de la eliminación
+                if (resultado > 0)
                 {
-                    MessageBox.Show("Producto eliminado con exito", "| Registro Producto",
+                    MessageBox.Show("Producto eliminado con éxito", "| Registro Producto",
                         MessageBoxButtons.OK, MessageBoxIcon.Information);
                 }
                 else
                 {
-                    MessageBox.Show("No se logro eliminar el Producto", "| Registro Producto",
+                    MessageBox.Show("No se logró eliminar el Producto", "| Registro Producto",
                         MessageBoxButtons.OK, MessageBoxIcon.Error);
                 }
 
+                // Actualiza el DataGridView con los datos más recientes
                 CargarProductos();
             }
         }
+
     }
 }
