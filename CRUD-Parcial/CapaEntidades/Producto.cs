9ef481c213ce88a2da8087dc51e9a14bedbe81c0
GeimeAlondra <105683683+GeimeAlondra@users.noreply.github.com>
2024-08-29 20:04:23 -0600

    Comentarios de CapaEntidades

--- CRUD-Parcial/CapaEntidades/Producto.cs
+++ CRUD-Parcial/CapaEntidades/Producto.cs
@@ -8,12 +8,25 @@ namespace CapaDatos
 {
     public class Producto
     {
+        // Identificador único del producto
         public int Id { get; set; }
+
+        // Nombre del producto
         public string Nombre { get; set; }
+
+        // Descripción del producto
         public string Descripcion { get; set; }
+
+        // Precio del producto de tipo decimal para manejar valores monetarios
         public decimal Precio { get; set; }
+
+        // Cantidad en stock del producto
         public int Stock { get; set; }
+
+        // Marca del producto
         public string Marca { get; set; }
+
+        // Categoría del producto
         public string Categoria { get; set; }
     }
 }
